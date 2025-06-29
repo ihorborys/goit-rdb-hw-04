d6d1d8c3150b0c2a8b892ef3a13b552cf786adf1
ihorborys <ihorborys@gmail.com>
2025-06-29 16:36:13 +0300

    Completed task 4c

---
+++ ASSETS/scripts/script_4c.sql
@@ -0,0 +1,19 @@
+SELECT *
+FROM
+    orders
+        INNER JOIN
+    customers ON customers.id = orders.customer_id
+        INNER JOIN
+    employees ON employees.employee_id = orders.employee_id
+        INNER JOIN
+    shippers ON shippers.id = orders.shipper_id
+        INNER JOIN
+    order_details ON order_details.order_id = orders.id
+        INNER JOIN
+    products ON products.id = order_details.product_id
+        INNER JOIN
+    categories ON categories.id = products.category_id
+        INNER JOIN
+    suppliers ON suppliers.id = products.supplier_id
+    
+WHERE employees.employee_id > 3 AND employees.employee_id < 11

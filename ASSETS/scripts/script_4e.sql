03d770b8d6575c301006a6b1c9dd9443b7ace84d
ihorborys <ihorborys@gmail.com>
2025-06-29 16:51:00 +0300

    Completed task 4e

---
+++ ASSETS/scripts/script_4e.sql
@@ -0,0 +1,23 @@
+SELECT 
+    categories.name,
+    COUNT(categories.name),
+    AVG(order_details.quantity),
+    order_details.quantity
+FROM
+    orders
+        INNER JOIN
+    customers ON customers.id = orders.customer_id
+        INNER JOIN
+    employees ON employees.employee_id = orders.employee_id
+        INNER JOIN
+    shippers ON shippers.id = orders.shipper_id
+        INNER JOIN
+    order_details ON order_details.order_id = orders.id
+        INNER JOIN
+    products ON products.id = order_details.product_id
+        INNER JOIN
+    categories ON categories.id = products.category_id
+        INNER JOIN
+    suppliers ON suppliers.id = products.supplier_id
+GROUP BY categories.name, order_details.quantity
+HAVING order_details.quantity > 21
